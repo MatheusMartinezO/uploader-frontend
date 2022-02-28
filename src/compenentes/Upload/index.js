00a827791b1ba391e7a1f0589a8652766f5e377f
MatheusDesenvolvedor <matheus234.maritnez.oliveira@gmail.com>
2022-02-28 18:01:43 -0300

    asad

--- src/compenentes/Upload/index.js
+++ src/compenentes/Upload/index.js
@@ -1,37 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-import Dropzone from "react-dropzone";
+import Dropzone from 'react-dropzone';
 
-import { DropContainer, UploadMessage } from "./styles";
+import {DropContainer,UploadMessage} from './styles';
 
 export default class Upload extends Component {
-  renderDragMessage = (isDragActive, isDragReject) => {
-    if (!isDragActive) {
-      return <UploadMessage>Arraste arquivos aqui...</UploadMessage>;
-    }
 
-    if (isDragReject) {
-      return <UploadMessage type="error">Arquivo não suportado</UploadMessage>;
-    }
+  renderDragMessage = (isDragActive,isDragReject) => {
+  
+   if(!isDragActive){
+     return <UploadMessage>Arraste arquivos aqui...</UploadMessage>
+   }
+   if(isDragReject){
+    return <UploadMessage type="error">arquivo não suportado</UploadMessage>
+   }
+   
+   return <UploadMessage type="success">solte os arquivos aqui...</UploadMessage>
 
-    return <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>;
   };
 
   render() {
-    const { onUpload } = this.props;
-
+    const {onUpload} = this.props;
     return (
       <Dropzone accept="image/*" onDropAccepted={onUpload}>
-        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+         {({getRootProps,getInputProps,isDragActive,isDragReject})=>(
           <DropContainer
-            {...getRootProps()}
-            isDragActive={isDragActive}
-            isDragReject={isDragReject}
+          {...getRootProps()}
+          isDragActive={isDragActive}
+          isDragReject={isDragReject}
           >
-            <input {...getInputProps()} />
-            {this.renderDragMessage(isDragActive, isDragReject)}
+            <input {...getInputProps()}/>
+
+            {this.renderDragMessage(isDragActive,isDragReject)}
           </DropContainer>
-        )}
+         )}
       </Dropzone>
     );
   }
