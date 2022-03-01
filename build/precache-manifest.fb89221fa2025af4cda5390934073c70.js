8ba7a0b2eb65f961b576c24ae8dcff8a513a0d23
MatheusDesenvolvedor <matheus234.maritnez.oliveira@gmail.com>
2022-03-01 03:42:21 -0300

    asdasddas

---
+++ build/precache-manifest.fb89221fa2025af4cda5390934073c70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da2cb302a30f635b7121",
+    "url": "/static/js/main.da2cb302.chunk.js"
+  },
+  {
+    "revision": "34f70c15ea33124e76f3",
+    "url": "/static/css/1.2dc11efb.chunk.css"
+  },
+  {
+    "revision": "34f70c15ea33124e76f3",
+    "url": "/static/js/1.34f70c15.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d987fbce0709b72e1c298cf9fbf00ac7",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react/cjs/react.development';
+import React, { Component } from 'react';
 import { uniqueId } from 'lodash';
 import filesize from 'filesize';
 import api from './services/api'
